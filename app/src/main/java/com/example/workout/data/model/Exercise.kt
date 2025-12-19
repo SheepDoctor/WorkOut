ebcc828c5741b1a1426648b45697d96ed32f4156
Bingwen Dong <118059589+SheepDoctor@users.noreply.github.com>
2025-12-19 23:56:59 +0800

    重置

--- app/src/main/java/com/example/workout/data/model/Exercise.kt
+++
@@ -1,9 +0,0 @@
-package com.example.workout.data.model
-
-data class Exercise(
-    val name: String,
-    val steps: List<String>,
-    val keyPoints: List<String>,
-    val illustrationUrl: String = ""
-)
-
