fd465581d7e7bf02c09e5ea8da843deb4e39335e
Bingwen Dong <118059589+SheepDoctor@users.noreply.github.com>
2025-12-19 23:08:39 +0800

    更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.kapt)
 }
 
 android {
@@ -38,7 +37,6 @@ android {
     }
     buildFeatures {
         viewBinding = true
-        dataBinding = true
     }
 }
 
